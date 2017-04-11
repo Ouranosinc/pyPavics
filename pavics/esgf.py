e004a4411025ceaef98363d978cca14557f2185e
Blaise Gauvin St-Denis <st-denis.blaise@ouranos.ca>
2017-04-11 08:30:15 -0400

    Improve search and wget execution

--- pavics/esgf.py
+++ pavics/esgf.py
@@ -1,6 +1,13 @@
 import os
+import sys
 import copy
-import http.client
+import json
+import itertools
+import pexpect
+try:
+    from http.client import HTTPConnection
+except ImportError:
+    from httplib import HTTPConnection
 try:
     from urllib.parse import quote_plus, urlparse
 except ImportError:
@@ -13,6 +20,10 @@ class InvalidSearchDict(Exception):
 
 
 class LimitExceeded(Exception):
+    pass
+
+
+class WgetStuck(Exception):
     pass
 
 
@@ -81,6 +92,15 @@ class ESGFSearchDict:
                 loose_components.append(loose_default)
         return loose_components
 
+    def list_split_candidates(self):
+        candidates = []
+        ns = []
+        for k, v in self.search.items():
+            if hasattr(v, '__iter__'):
+                candidates.append(k)
+                ns.append(len(v))
+        return [x for (y, x) in sorted(zip(ns, candidates))]
+
     def split_by_elements(self, split_components, elements_defaults=None):
         """Split search dictionary into multiple search dictionaries.
 
@@ -96,24 +116,23 @@ class ESGFSearchDict:
             return [copy.deepcopy(self)]
         if elements_defaults is None:
             elements_defaults = {}
-        elements_list = {}
+        elements_lists = []
         for component in split_components:
-            elements_list[component] = []
+            elements_lists.append([])
             c1 = component in self.search
             if c1 and self.search[component] is not None:
                 if hasattr(self.search[component], '__iter__'):
-                    elements_list[component].extend(self.search[component])
+                    elements_lists[-1].extend(self.search[component])
                 else:
-                    elements_list.append(self.search[component])
+                    elements_lists[-1].append(self.search[component])
             elif component in elements_defaults:
-                elements_list[component].extend(elements_defaults[component])
+                elements_lists[-1].extend(elements_defaults[component])
 
-        warp = genfunc.multiple_for(split_components, **elements_list)
         sub_ds = []
-        for elements_values in warp:
+        for elements_tuple in itertools.product(*elements_lists):
             sub_d = copy.deepcopy(self)
-            for component in split_components:
-                sub_d.search[component] = elements_values[component]
+            for i, component in enumerate(split_components):
+                sub_d.search[component] = elements_tuple[i]
             sub_ds.append(sub_d)
         return sub_ds
 
@@ -311,7 +330,7 @@ class ESGFSearchDict:
         one_search_url = self.search_url(esgf_search_url, facets, shards,
                                          offset, limit, fields, output_format)
         parsed_url = urlparse(one_search_url)
-        conn = http.client.HTTPConnection(parsed_url.hostname)
+        conn = HTTPConnection(parsed_url.hostname)
         search_path_start = one_search_url.find(parsed_url.hostname) + \
             len(parsed_url.hostname)
         conn.request("GET", one_search_url[search_path_start:])
@@ -319,6 +338,60 @@ class ESGFSearchDict:
         esgf_response = r1.read()
         conn.close()
         return esgf_response
+
+    def dataset_search_with_split_retries(self, esgf_search_url, facets=None,
+                                          shards=None, offset=None, limit=None,
+                                          fields=None):
+        json_responses = [None]
+        overflow = [-1]
+        sub_ds = [self]
+        while True:
+            for i, oflag in enumerate(overflow):
+                if oflag == -1:
+                    r = sub_ds[i].dataset_search(esgf_search_url,
+                                                 facets=facets,
+                                                 shards=shards, offset=offset,
+                                                 limit=limit, fields=fields,
+                                                 output_format='json')
+                    json_responses[i] = json.loads(r)
+                    num_found = json_responses[i]['response']['numFound']
+                    num_returned = len(json_responses[i]['response']['docs'])
+                    if num_found > num_returned:
+                        overflow[i] = 1
+                    else:
+                        overflow[i] = 0
+
+            overflow_pop = []
+            for i, oflag in enumerate(overflow):
+                if oflag == 1:
+                    overflow_pop.append(i)
+                    split_candidates = sub_ds[i].list_split_candidates()
+                    if not split_candidates:
+                        raise LimitExceeded()
+                    sc = split_candidates[0]
+                    split_ds = sub_ds[i].split_by_elements([sc])
+                    sub_ds.extend(split_ds)
+                    json_responses.extend([None] * len(split_ds))
+                    overflow.extend([-1] * len(split_ds))
+            for i in overflow_pop[::-1]:
+                dummy = sub_ds.pop(i)
+                dummy = json_responses.pop(i)
+                dummy = overflow.pop(i)
+
+            if -1 not in overflow:
+                break
+
+        n = 0
+        for json_response in json_responses[1:]:
+            add_docs = json_response['response']['docs']
+            # Unfortunately, need to check for duplicates since separate
+            # variable searches can lead to the same dataset...
+            for add_doc in add_docs:
+                if add_doc not in json_responses[0]['response']['docs']:
+                    json_responses[0]['response']['docs'].append(add_doc)
+                    n += 1
+        json_responses[0]['response']['numFound'] += n
+        return json_responses[0], sub_ds
 
     def datasets_from_d(self, esgf_server, search_type='Dataset', limit=None,
                         verbose=True, log_file=None):
@@ -377,7 +450,7 @@ class ESGFSearchDict:
             wget_path = "{0}&limit={1}".format(wget_path, str(limit))
             # wget_path = wget_path + "&limit=%s" % (str(limit),)
         # conn = httplib.HTTPConnection(esgf_server)
-        conn = http.client.HTTPConnection(esgf_server)
+        conn = HTTPConnection(esgf_server)
         conn.request("GET", wget_path)
         r1 = conn.getresponse()
         wget_script = r1.read()
@@ -389,3 +462,92 @@ class ESGFSearchDict:
         f1 = open(wget_file, 'w')
         f1.write(wget_script)
         f1.close()
+
+
+def keyword_string_or_file(string_or_file):
+    """Get string from either a string or file.
+
+    Parameters
+    ----------
+    string_or_file : string or file
+
+    Returns
+    -------
+    out : string
+        the input string, or the text found in the given file.
+
+    """
+
+    if os.path.isfile(string_or_file):
+        f1 = open(string_or_file, 'r')
+        string_or_file = f1.read().rstrip('\n')
+        f1.close()
+    return string_or_file
+
+
+def wget_execution(wget_script, myopenid, myopenpw, wget_timeout=172800,
+                   chdir=None, wget_args=[]):
+    """wget script execution.
+
+    Parameters
+    ----------
+    wget_script : string
+    myopenid : string
+    myopenpw : string or file
+    wget_timeout : int
+    chdir : string
+
+    """
+
+    os.chmod(wget_script, 0744)
+    if chdir is not None:
+        cwd = os.getcwd()
+        os.chdir(chdir)
+    child = pexpect.spawn(wget_script, wget_args, logfile=sys.stdout)
+    if chdir is not None:
+        os.chdir(cwd)
+    if '-H' in wget_args:
+        i = child.expect(["Enter your openid*\?*", pexpect.EOF,
+                          pexpect.TIMEOUT],
+                         timeout=120)
+        if i == 0:
+            child.sendline(myopenid)
+            j = child.expect(["Enter password\?*", pexpect.EOF,
+                              pexpect.TIMEOUT],
+                             timeout=60)
+            if j == 0:
+                child.logfile = None
+                myopenpw = keyword_string_or_file(myopenpw)
+                child.sendline(myopenpw)
+                del myopenpw
+                child.logfile = sys.stdout
+            else:
+                raise NotImplementedError("Gave a username but no password?")
+        elif i == 1:
+            return
+    else:
+        i = child.expect(["Please give your OpenID*\?*", pexpect.EOF,
+                          pexpect.TIMEOUT],
+                         timeout=120)
+        if i == 0:
+            child.sendline(myopenid)
+            j = child.expect(["MyProxy Password\?*", pexpect.EOF,
+                              pexpect.TIMEOUT],
+                             timeout=60)
+            if j == 0:
+                child.logfile = None
+                myopenpw = genfunc.keyword_string_or_file(myopenpw)
+                child.sendline(myopenpw)
+                del myopenpw
+                child.logfile = sys.stdout
+            else:
+                raise NotImplementedError("Gave a username but no password?")
+        elif i == 1:
+            return
+    i = child.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=60)
+    t = 60
+    while i == 1:
+        i = child.expect([pexpect.EOF, pexpect.TIMEOUT], timeout=60)
+        t += 60
+        if t > wget_timeout:
+            raise WgetStuck()
--- pavics/proofofconcept.py
+++ pavics/proofofconcept.py
@@ -1,13 +1,18 @@
+import os
 import esgf
 import json
+import copy
 
 esgf_server = 'https://esgf-node.llnl.gov'
 esgf_search_url = 'https://esgf-node.llnl.gov/esg-search'
 
+search_paths = ['/dmf2/scenario/external_data/CMIP5',
+                '/dmf2/scenario/broken/cmip5_download']
+
 # type is one of Dataset, File or Aggregation
 # distrib will determine if searching only one the selected node or all
 # the federated nodes.
-search_dict = esgf.ESGFSearchDict({'type':'File',
+search_dict = esgf.ESGFSearchDict({'type':'Dataset',
                                    'distrib':'true',
                                    'project':'CMIP5',
                                    'institute':None,
@@ -21,17 +26,114 @@ search_dict = esgf.ESGFSearchDict({'type':'File',
                                    'data_node':None,
                                   })
 
-myurl = search_dict.search_url(esgf_search_url)
+#myurl = search_dict.search_url(esgf_search_url)
 
-json_res = search_dict.dataset_search(esgf_search_url,limit=1000,
-                                      output_format='json')
+json_res = search_dict.dataset_search_with_split_retries(esgf_search_url,
+                                                         limit=1000)
 
-j1 = json.loads(json_res)
+data_nodes = []
+for i in range(j1['response']['numFound']):
+    if j1['response']['docs'][i]['data_node'] not in data_nodes:
+        data_nodes.append(j1['response']['docs'][i]['data_node'])
 
-j1['response']['numFound']
-len(j1['response']['docs'])
+print(data_nodes)
+
+split_ds = search_dict.split_by_elements(['data_node'],
+                                         {'data_node':data_nodes})
+
+json_results = []
+n = 0
+for split_d in split_ds:
+    split_d.search['type'] = 'File'
+    fn = split_d.dataset_search_with_split_retries
+    json_results.append(fn(esgf_search_url,limit=1000))
+    n += len(json_results[-1]['response']['docs'])
+
+print("Found {0} files.".format(str(n)))
 
 # If numFound > len, we need to split the request, since this is Solr and
 # there are concurrency issues with pagination
 # Note that Solr default sort order is oldest document first (editing a
 # document renews it completely)
+
+
+
+#for data_node in data_nodes:
+    #sub_search_dict = copy.deepcopy(search_dict)
+    #sub_search_dict.search['data_node'] = data_node
+    #sub_search_dict.search['type'] = 'File'
+    #json_res = sub_search_dict.dataset_search(esgf_search_url, limit=1000,
+                                              #output_format='json')
+    #subj1 = json.loads(json_res)
+    
+myfiles = {}
+for search_path in search_paths:
+    for root, dirs, files in os.walk(search_path):
+        for one_file in files:
+            if one_file in myfiles:
+                myfiles[one_file].append(root)
+            else:
+                myfiles[one_file] = [root]
+
+
+def checksum_large_file(file1, block_size=2**20, hashlib_fn=hashlib.sha256):
+    f1 = open(file1)
+    checksum = hashlib_fn()
+    while True:
+        data1 = f1.read(block_size)
+        if not data1:
+            break
+        checksum.update(data1)
+    f1.close()
+    return checksum.hexdigest()
+
+class FileOnDisk():
+    def __init__(self, path):
+        self.path = path
+        self.basename = os.path.basename(path)
+        self.size = os.path.getsize(path)
+    def md5():
+        return checksum_large_file(self.path, hashlib_fn=hashlib.md5)
+    def sha256():
+        return checksum_large_file(self.path, hashlib_fn=hashlib.sha256)        
+
+class ESGFFile():
+    def __init__(self, esgf_doc):
+        self.doc = esgf_doc
+        self.data_node = esgf_doc['data_node']
+        self.title = esgf_doc['title']
+        self.size = esgf_doc['size']
+        if 'checksum' in esgf_doc:
+            self.checksum = esgf_doc['checksum']
+        else:
+            self.checksum = None
+        if 'checksum_type' in esgf_doc:
+            self.checksum_type = esgf_doc['checksum_type'].upper()
+        else:
+            # Assuming default checksum type is SHA256
+            self.checksum_type = 'SHA256'
+
+def file_check(json_results, files_on_disk):
+    files_status = {}
+    for json_result in json_results:
+        for one_doc in json_result['response']['docs']:
+            file_title = one_doc['title']
+            if file_title not in files_status:
+                files_status[file_title] = {}
+            if file_title in files_on_disk:
+                warp = copy.deepcopy(files_on_disk[file_title])
+                files_status[file_title]['local_paths'] = warp
+            if 'data_nodes' in files_status[file_title]:
+                warp = one_doc['data_node']
+                files_status[file_title]['data_nodes'].append(warp)
+            else:
+                files_status[file_title]['data_nodes'] = [one_doc['data_node']]
+    for one_file, file_args in files_status.items():
+        
+        
+
+for json_result in json_results:
+    for one_doc in json_result['response']['docs']:
+        one_doc['title']
+        ond_doc['checksum']
+        one_doc['checksum_type']
