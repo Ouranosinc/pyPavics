Fix various spatial weighted average issues

pavics/geogrid.py
@@ -1,7 +1,9 @@
 import logging
+import copy
 
 import numpy as np
-from shapely.geometry import Point, Polygon, LineString
+from shapely.geometry import Point, LineString, Polygon, MultiPoint,\
+                             MultiLineString, MultiPolygon, GeometryCollection
 from shapely import iterops
 from shapely.ops import cascaded_union
 
@@ -113,6 +115,39 @@ def detect_grid(lon, lat, lon_dimensions=None, lat_dimensions=None):
             elif lon.size == lat.size:
                 return 'list_of_2d_points'
     raise GeogridError("Unknown grid.")
+
+
+def grid_fix_longitudes(longitudes, warp_longitude):
+    """Constrain longitudes to the 360 degrees below a given longitude.
+
+    Parameters
+    ----------
+    longitudes - numpy array
+    warp_longitude - float
+
+    Returns
+    -------
+    out - numpy array
+
+    Examples
+    --------
+    To return longitudes between 0 and 360:
+    >> grid_fix_longitudes(longitudes,360.0)
+
+    To return longitudes between -180 and 180:
+    >> grid_fix_longitudes(longitudes,180.0)
+
+    """
+
+    over_warp = longitudes >= warp_longitude
+    while over_warp.sum() > 0:
+        longitudes = longitudes-360.0*over_warp
+        over_warp = longitudes >= warp_longitude
+    under_warp = longitudes < warp_longitude-360.0
+    while under_warp.sum() > 0:
+        longitudes = longitudes+360.0*under_warp
+        under_warp = longitudes < warp_longitude-360.0
+    return longitudes
 
 
 def rectilinear_centroids_to_vertices(x, y, forced_bounds=False,
@@ -822,6 +857,7 @@ def _inpolygon_from_list_of_points(lon_points, lat_points, geometry):
             valid_indices.append(i)
     return valid_indices
 
+
 def _inpolygon_from_rectilinear_vertices(lon_vertices, lat_vertices, geometry):
     """Find the points from a rectilinear grid defined by its vertices that 
        fall in a geometry.
@@ -853,10 +889,11 @@ def _inpolygon_from_rectilinear_vertices(lon_vertices, lat_vertices, geometry):
                 inpolygon_list[1].append(j)
     return (np.array(inpolygon_list[0]),np.array(inpolygon_list[1]))
 
+
 def _inpolygon_from_rectilinear_centroids(lon_centroids, lat_centroids,
                                           geometry, lon_vertices=None,
                                           lat_vertices=None):
-    """Find the points from a rectilinear grid defined by its centroids that 
+    """Find the points from a rectilinear grid defined by its centroids that
        fall in a geometry.
 
     Parameters
@@ -915,7 +952,7 @@ def _inpolygon_from_irregular_vertices(lon_vertices, lat_vertices, geometry):
 
 def _inpolygon_from_irregular_centroids(lon_centroids, lat_centroids, geometry,
                                         lon_vertices=None, lat_vertices=None):
-    """Find the points from an irregular grid defined by its centroids that 
+    """Find the points from an irregular grid defined by its centroids that
        fall in a geometry.
 
     Parameters
@@ -986,7 +1023,25 @@ def subdomain_grid_slices_or_points_indices(lon, lat, geometry, grid_type=None,
 subdom_slice_or_i = subdomain_grid_slices_or_points_indices
 
 
-def lon_lat_polygon_area(lon_lat_polygon,lon_0=0):
+def expand_polygon(polygon, n=1):
+    # add more vertices to a polygon
+    while n:
+        exterior1 = list(polygon.boundary.coords)
+
+        lon1 = []
+        lat1 = []
+        xys = []
+        for i in range(len(exterior1)-1):
+            xys.append((exterior1[i][0], exterior1[i][1]))
+            xys.append(((exterior1[i][0]+exterior1[i+1][0])/2.0,
+                        (exterior1[i][1]+exterior1[i+1][1])/2.0))
+        xys.append((exterior1[-1][0], exterior1[-1][1]))
+        polygon = Polygon(xys)
+        n -= 1
+    return polygon
+
+
+def lon_lat_polygon_area(lon_lat_polygon,lon_0=None):
     """Area of a polygon in lon/lat projection.
 
     Parameters
@@ -1003,6 +1058,8 @@ def lon_lat_polygon_area(lon_lat_polygon,lon_0=0):
 
     from mpl_toolkits.basemap import Basemap
 
+    if lon_0 is None:
+        lon_0 = lon_lat_polygon.centroid.x
     m1 = Basemap(projection='moll',lon_0=lon_0)
     exterior1 = list(lon_lat_polygon.boundary.coords)
 
@@ -1042,9 +1099,25 @@ def spatial_weighted_average(lon_vertices, lat_vertices, geometry, field,
                                 (lon_vertices[i+1], lat_vertices[j]),
                                 (lon_vertices[i+1], lat_vertices[j+1]),
                                 (lon_vertices[i], lat_vertices[j+1])])
-                cell_area = lon_lat_polygon_area(pol1)
-                area = lon_lat_polygon_area(pol1.intersection(geometry))
-                weights[j,i] = area/float(cell_area)
+                # adding vertices to polygon in case it's a large grid
+                cell_area = lon_lat_polygon_area(expand_polygon(pol1,5))
+                intersection = pol1.intersection(geometry)
+                if isinstance(intersection, LineString):
+                    area = 0
+                elif isinstance(intersection, Point):
+                    area = 0
+                elif isinstance(intersection, GeometryCollection):
+                    if len(intersection) == 0:
+                        # empty geometry collection
+                        area = 0
+                    else:
+                        # what would that be?
+                        raise NotImplementedError()
+                else:
+                    area = lon_lat_polygon_area(expand_polygon(intersection,5))
+                it = i-slices[0].start
+                jt = j-slices[1].start
+                weights[jt,it] = area/float(cell_area)
     elif grid_type == 'irregular_2d_vertices':
         weights = np.zeros([slices[0].stop-slices[0].start,
                             slices[1].stop-slices[1].start])
@@ -1054,15 +1127,25 @@ def spatial_weighted_average(lon_vertices, lat_vertices, geometry, field,
                                 (lon_vertices[i+1,j], lat_vertices[i+1,j]),
                                 (lon_vertices[i+1,j+1], lat_vertices[i+1,j+1]),
                                 (lon_vertices[i,j+1], lat_vertices[i,j+1])])
-                cell_area = lon_lat_polygon_area(pol1)
+                # adding vertices to polygon in case it's a large grid
+                cell_area = lon_lat_polygon_area(expand_polygon(pol1,5))
                 intersection = pol1.intersection(geometry)
                 if isinstance(intersection, LineString):
                     area = 0
                 elif isinstance(intersection, Point):
                     area = 0
+                elif isinstance(intersection, GeometryCollection):
+                    if len(intersection) == 0:
+                        # empty geometry collection
+                        area = 0
+                    else:
+                        # what would that be?
+                        raise NotImplementedError()
                 else:
-                    area = lon_lat_polygon_area(intersection)
-                weights[i,j] = area/float(cell_area)
+                    area = lon_lat_polygon_area(expand_polygon(intersection,5))
+                it = i-slices[0].start
+                jt = j-slices[1].start
+                weights[it,jt] = area/float(cell_area)
     weights = weights/weights.sum()
     if spatial_indices:
         if grid_type == 'rectilinear_2d_vertices':
@@ -1097,3 +1180,61 @@ def spatial_weighted_average(lon_vertices, lat_vertices, geometry, field,
             field = field[slices[0],slices[1]]
         weighted_average_field = (field*weights).sum()
     return weighted_average_field
+
+
+def shapely_fix_longitudes(geometry,warp_longitude):
+    """Constrain longitudes of a shapely geometry.
+
+    Parameters
+    ----------
+    geometry - Shapely geometry
+    warp_longitude - float
+
+    Returns
+    -------
+    out - Shapely geometry
+
+    """
+
+    if isinstance(geometry,Point):
+        new_x = grid_fix_longitudes(np.array([geometry.x]),warp_longitude)
+        if geometry.has_z:
+            return Point(new_x,geometry.y,geometry.z)
+        else:
+            return Point(new_x,geometry.y)
+    elif isinstance(geometry,LineString):
+        coords = np.asarray(geometry)
+        coords[:,0] = grid_fix_longitudes(coords[:,0],warp_longitude)
+        return LineString(coords)
+    elif isinstance(geometry,Polygon):
+        coords = np.asarray(geometry.exterior)
+        coords[:,0] = grid_fix_longitudes(coords[:,0],warp_longitude)
+        new_interiors = []
+        for interior in geometry.interiors:
+            new_interiors.append(np.asarray(interior))
+            new_interiors[-1][:,0] = grid_fix_longitudes(
+                new_interiors[-1][:,0], warp_longitude)
+        return Polygon(coords,new_interiors)
+    elif isinstance(geometry,MultiPoint):
+        coords = np.asarray(geometry)
+        coords[:,0] = grid_fix_longitudes(coords[:,0],warp_longitude)
+        return MultiPoint(coords)
+    elif isinstance(geometry,MultiLineString):
+        new_geom = []
+        for linestring in geometry.geoms:
+            new_geom.append(np.asarray(linestring))
+            new_geom[-1][:,0] = grid_fix_longitudes(new_geom[-1][:,0],
+                                                    warp_longitude)
+        return MultiLineString(new_geom)
+    elif isinstance(geometry,MultiPolygon):
+        new_geom = []
+        for polygon in geometry.geoms:
+            coords = np.asarray(polygon.exterior)
+            coords[:,0] = grid_fix_longitudes(coords[:,0],warp_longitude)
+            new_in = []
+            for interior in polygon.interiors:
+                new_in.append(np.asarray(interior))
+                new_in[-1][:,0] = grid_fix_longitudes(new_in[-1][:,0],
+                                                      warp_longitude)
+            new_geom.append((copy.deepcopy(coords),copy.deepcopy(new_in)))
+        return MultiPolygon(new_geom)

tests/test_ncgeo.py
@@ -469,12 +469,111 @@ class TestNcGeo(unittest.TestCase):
         self.assertEqual(nclon.size, 1)
         self.assertEqual(nclat.size, 1)
         self.assertEqual(nclon[0], 105)
-        #self.assertEqual(nclon[-1], 140)
         self.assertEqual(nclat[0], -9)
-        #self.assertEqual(nclat[-1], 40)
         self.assertEqual(ncvar.shape, (1, 1))
         self.assertEqual(ncvar[0,0], 203)
-        #self.assertEqual(ncvar[-1,-1], 34)
+
+    def test_spatial_weighted_average_02(self):
+        # regular centroids 0-360
+        pavnc.create_dummy_netcdf(
+            self.dummy_file_1, use_time=False, lat_size=6, lon_size=12,
+            lon_values=[15, 45, 75, 105, 135, 165, 195, 225, 255, 285, 315,
+                        345],
+            lat_values=[-75, -45, -15, 15, 45, 75],
+            var_values=[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                        [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+                         111],
+                        [200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210,
+                         211],
+                        [300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310,
+                         311],
+                        [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410,
+                         411],
+                        [500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510,
+                         511]])
+        geometry = Polygon([(180, -30), (180, 30), (210, 60), (240, 30),
+                            (240, -30)])
+        ncgeo.spatial_weighted_average(self.dummy_file_1, self.dummy_file_2,
+                                       geometry)
+        nc = netCDF4.Dataset(self.dummy_file_2, 'r')
+        nclon = nc.variables['lon']
+        nclat = nc.variables['lat']
+        ncvar = nc.variables['dummy']
+        self.assertEqual(nclon.size, 1)
+        self.assertEqual(nclat.size, 1)
+        self.assertEqual(nclon[0], 210)
+        self.assertEqual(nclat[0], 8.0)
+        self.assertEqual(ncvar.shape, (1, 1))
+        self.assertAlmostEqual(ncvar[0,0], 286.5, delta=3)
+
+    def test_spatial_weighted_average_03(self):
+        # regular centroids 0-360
+        # polygon with a different reference system
+        pavnc.create_dummy_netcdf(
+            self.dummy_file_1, use_time=False, lat_size=6, lon_size=12,
+            lon_values=[15, 45, 75, 105, 135, 165, 195, 225, 255, 285, 315,
+                        345],
+            lat_values=[-75, -45, -15, 15, 45, 75],
+            var_values=[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                        [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110,
+                         111],
+                        [200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210,
+                         211],
+                        [300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310,
+                         311],
+                        [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410,
+                         411],
+                        [500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510,
+                         511]])
+        geometry = Polygon([(-180, -30), (-180, 30), (-150, 60), (-120, 30),
+                            (-120, -30)])
+        ncgeo.spatial_weighted_average(self.dummy_file_1, self.dummy_file_2,
+                                       geometry)
+        nc = netCDF4.Dataset(self.dummy_file_2, 'r')
+        nclon = nc.variables['lon']
+        nclat = nc.variables['lat']
+        ncvar = nc.variables['dummy']
+        self.assertEqual(nclon.size, 1)
+        self.assertEqual(nclat.size, 1)
+        self.assertEqual(nclon[0], 210)
+        self.assertEqual(nclat[0], 8.0)
+        self.assertEqual(ncvar.shape, (1, 1))
+        self.assertAlmostEqual(ncvar[0,0], 286.5, delta=3)
+
+    def test_spatial_weighted_average_04(self):
+        # irregular centroids 0-360
+        mylon = np.array([[3, 7, 11, 15, 19, 23],
+                          [5, 9, 13, 17, 21, 25],
+                          [7, 11, 15, 19, 23, 27],
+                          [9, 13, 17, 21, 25, 29],
+                          [11, 15, 19, 23, 27, 31]])
+        mylat = np.array([[2.5, 3.5, 4.5, 5.5, 6.5, 7.5],
+                          [6.5, 7.5, 8.5, 9.5, 10.5, 11.5],
+                          [10.5, 11.5, 12.5, 13.5, 14.5, 16.5],
+                          [14.5, 15.5, 16.5, 17.5, 18.5, 19.5],
+                          [18.5, 19.5, 20.5, 21.5, 22.5, 23.5]])
+        pavnc.create_dummy_netcdf(
+            self.dummy_file_1, use_time=False, use_lat=False, use_lon=False,
+            use_ycxc=True, yc_size=5, xc_size=6,
+            lon_values=mylon, lat_values=mylat,
+            var_values=[[0, 1, 2, 3, 4, 5],
+                        [10, 11, 12, 13, 14, 15],
+                        [20, 21, 22, 23, 24, 25],
+                        [30, 31, 32, 33, 34, 35],
+                        [40, 41, 42, 43, 44, 45]])
+        geometry = Polygon([(17, 10), (17, 16), (21, 16), (21, 10)])
+        ncgeo.spatial_weighted_average(self.dummy_file_1, self.dummy_file_2,
+                                       geometry)
+        nc = netCDF4.Dataset(self.dummy_file_2, 'r')
+        nclon = nc.variables['lon']
+        nclat = nc.variables['lat']
+        ncvar = nc.variables['dummy']
+        self.assertEqual(nclon.size, 1)
+        self.assertEqual(nclat.size, 1)
+        self.assertEqual(nclon[0], 19)
+        self.assertEqual(nclat[0], 13)
+        self.assertEqual(ncvar.shape, (1, 1))
+        self.assertAlmostEqual(ncvar[0,0], 21.4, delta=0.2)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestNcGeo)
 