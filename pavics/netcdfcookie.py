d5be70b16b059af577f2dbca2eaa031355da7364
David Byrns <david.byrns@crim.ca>
2017-11-23 20:54:48 -0500

    Handle properly the case where no headers are given

--- pavics/netcdfcookie.py
+++ pavics/netcdfcookie.py
@@ -12,7 +12,7 @@ class NetCDFCookie:
     def get_auth_cookie(self, headers):
         try:
             return dict(cookie.split('=') for cookie in headers['Cookie'].split(';'))
-        except KeyError:
+        except (KeyError, TypeError):
             # No token... will be anonymous
             return None
 
@@ -23,17 +23,17 @@ class NetCDFCookie:
             with open(self.daprc_fn, 'w') as f:
                 f.write('HTTP.COOKIEJAR = {0}'.format(self.auth_cookie_fn))
 
-        with open(self.auth_cookie_fn, 'w') as f:
-            for opendap_hostname in self.opendap_hostnames:
-                for key, value in self.cookie.items():
-                    f.write('{domain}\t{access_flag}\t{path}\t{secure}\t{expiration}\t{name}\t{value}\n'.format(
-                        domain=opendap_hostname,
-                        access_flag='FALSE',
-                        path='/',
-                        secure='FALSE',
-                        expiration=0,
-                        name=key,
-                        value=value))
+            with open(self.auth_cookie_fn, 'w') as f:
+                for opendap_hostname in self.opendap_hostnames:
+                    for key, value in self.cookie.items():
+                        f.write('{domain}\t{access_flag}\t{path}\t{secure}\t{expiration}\t{name}\t{value}\n'.format(
+                            domain=opendap_hostname,
+                            access_flag='FALSE',
+                            path='/',
+                            secure='FALSE',
+                            expiration=0,
+                            name=key,
+                            value=value))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.cookie:
